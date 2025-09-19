9a574c0f3dba1b9ddaeac73a32e100d0900964bd
metepg <mete.guneysel@gmail.com>
2025-09-19 11:07:31 +0300

    Fix tests

--- client/src/app/app.component.spec.ts
+++ client/src/app/app.component.spec.ts
@@ -1,10 +1,18 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { MessageService } from 'primeng/api';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppComponent],
+      providers: [
+        provideHttpClient(),
+        provideHttpClientTesting(),
+        MessageService
+      ]
     }).compileComponents();
   });
 
--- client/src/app/src/app/features/time-entry-form/time-entry-form.component.spec.ts
+++ client/src/app/src/app/features/time-entry-form/time-entry-form.component.spec.ts
@@ -1,6 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TimeEntryFormComponent } from './time-entry-form.component';
+import { MessageService } from 'primeng/api';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 describe('TimeEntryFormComponent', () => {
   let component: TimeEntryFormComponent;
@@ -8,9 +12,14 @@ describe('TimeEntryFormComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [TimeEntryFormComponent]
-    })
-    .compileComponents();
+      imports: [TimeEntryFormComponent],
+      providers: [
+        provideAnimations(),
+        provideHttpClient(),
+        provideHttpClientTesting(),
+        MessageService
+      ]
+    }).compileComponents();
 
     fixture = TestBed.createComponent(TimeEntryFormComponent);
     component = fixture.componentInstance;
