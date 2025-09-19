317f09ff1d79d3df94d6b68d1d5433706bec32c5
metepg <mete.guneysel@gmail.com>
2025-09-19 09:37:45 +0300

    Fix local environment

--- server/src/main/resources/db/addAdmin.sh
+++
@@ -1,4 +0,0 @@
-export PGPASSWORD=postgres
-psql -U postgres -d hours -c "INSERT INTO users (username, password, enabled) VALUES ('admin', '\$2a\$10\$yY0BX0t4asaqnFDe4HM2h.XjFkmYmOip2yr7GKtosx1J4hQR25Ar2', true);"
-psql -U postgres -d hours -c "INSERT INTO authorities (username, authority) VALUES ('admin', 'ADMIN');"
-
---
+++ server/src/main/resources/db/addAdmin.sql
@@ -0,0 +1,3 @@
+INSERT INTO users (username, password, enabled) VALUES ('admin', '$2a$10$yY0BX0t4asaqnFDe4HM2h.XjFkmYmOip2yr7GKtosx1J4hQR25Ar2', true);
+INSERT INTO authorities (username, authority) VALUES ('admin', 'ADMIN');
+
