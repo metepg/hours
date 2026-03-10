60d1eba7b2519814f82dee810fe9a626341bd033
Mete Guneysel <52155024+metepg@users.noreply.github.com>
2026-03-10 10:42:10 +0200

    Modify time inserting
    * Add mvn wrapper
    * Add docker-compose.yml
    * Fix script to add user (addAdmin.sh)

---
+++ server/src/main/resources/db/addAdmin.sh
@@ -0,0 +1,4 @@
+export PGPASSWORD=postgres
+docker exec -it hours_db psql -U postgres -d hours -c "INSERT INTO users (username, password, enabled) VALUES ('admin', '\$2a\$10\$yY0BX0t4asaqnFDe4HM2h.XjFkmYmOip2yr7GKtosx1J4hQR25Ar2', true);"
+docker exec -it hours_db psql -U postgres -d hours -c "INSERT INTO authorities (username, authority) VALUES ('admin', 'ADMIN');"
+
